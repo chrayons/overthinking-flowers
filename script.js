9efeeb2622a89d38531268061548fd758111149c
chrayons <christina.tn.lu@gmail.com>
2025-09-17 12:47:57 -0400

    Add basic project structure

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Flower garden loading...");
+
+// Test data for one flower
+const testFlower = {
+    sadness: 83,
+    joy: 12,
+    fear: 25
+};
+
+console.log("Test flower data:", testFlower);
